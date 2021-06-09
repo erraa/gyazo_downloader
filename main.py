138284ca33794260ba42d9223c2f7f23a32ae1a9
esaren <esaren@cisco.com>
2021-06-09 15:00:07 +0200

    fixed python 3 and if no image url

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ def main(settings):
         print("Need to specify token in ./client.yml")
 
     client = Api(access_token=settings['token'])
-    for i in xrange(1, 10000):
+    for i in range(1, 10000):
         images = client.get_image_list(per_page=100, page=i)
         if len(images) == 0:
             break
@@ -18,12 +18,18 @@ def main(settings):
             os.mkdir("./images")
 
         for image in images:
+            if not image.url:
+                continue
             r = requests.get(image.url, stream=True)
             if r.status_code == 200:
-                with open("images/{}.png".format(image.image_id), "wb") as imgfile:
-                    print("writing to file {}.png".format(image.image_id))
-                    for chunk in r:
-                        imgfile.write(chunk)
+                try:
+                    with open("images/{}.png".format(image.image_id), "xb") as imgfile:
+                        print("writing to file {}.png".format(image.image_id))
+                        for chunk in r:
+                            imgfile.write(chunk)
+                except FileExistsError:
+                    print("Skipping image_id {} as it already exists".format(image.image_id))
+                    continue
 
 if __name__ == '__main__':
     with open("./client.yml", "r") as f:
